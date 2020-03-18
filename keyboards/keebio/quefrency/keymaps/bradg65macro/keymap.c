ef8a350b53ccf437777605c0b6d542e5b46e8784
Bradley Guest <bguest@leaporbit.com>
2020-03-18 15:21:41 -0400

    Added 65 w/ macro layout

--- keyboards/keebio/quefrency/keymaps/bradg65macro/keymap.c
+++ keyboards/keebio/quefrency/keymaps/bradg65macro/keymap.c
@@ -8,6 +8,7 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _BASE 0
 #define _FN1 1
+#define _FN2 2
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -15,19 +16,27 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_65_with_macro(
-    KC_F1,   KC_F2,   KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_BSPC, KC_TILD, \
-    KC_F3,   KC_F4,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
+    KC_F11,  KC_F12,  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_BSPC, KC_GRV, \
+    KC_F9,   KC_F10,  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
     KC_F5,   KC_F6,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_END, \
-    KC_F7,   KC_F8,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   _______, \
-    KC_F9,   KC_F10,  KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1),KC_SPC,           KC_SPC,  _______, KC_RALT, KC_RCTL, _______, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_F3,   KC_F4,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   _______, \
+    KC_F1,   KC_F2,   KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1),KC_SPC,           KC_SPC,  _______, MO(_FN2),KC_RCTL, _______, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
-    _______, _______, KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______, \
+    _______, _______, KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, _______, \
     _______, _______, RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, KC_TILD, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, KC_MS_U, _______, \
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
+  ),
+
+  [_FN2] = LAYOUT_65_with_macro(
+    _______, _______, KC_TILD, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_VOLD, KC_VOLU, KC_MPLY, KC_MSTP,  KC_F23,  KC_F24,  KC_DEL,  KC_BSPC, _______, \
+    _______, _______, RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, KC_MPRV, KC_MNXT, KC_MRWD, KC_MFFD, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BRIU, _______, \
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, KC_BRID, _______
   )
 };
 
